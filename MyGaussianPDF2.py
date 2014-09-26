e52d905b186cf10ee75703238fc44fcee63c0e2d
Sam Stern <s1134468@cplab092.ph.ed.ac.uk>
2014-09-26 15:52:06 +0100

    fixed object orientation

---
+++ MyGaussianPDF2.py
@@ -0,0 +1,79 @@
+import numpy as np
+import pylab as pl
+import matplotlib.pyplot as plt 
+import math
+import sys
+import numpy.random as rand 
+sys.setrecursionlimit(10000)
+
+class PDF:
+
+	def __init__(self,a,b):
+		self.a=a
+		self.b=b
+	
+	@staticmethod	
+	def toRangeAB(a,b,x):
+		return a+(b-a)*x
+
+	@staticmethod
+	def toRangeFmax(fmax,y):
+		return fmax*y
+
+	def evaluate(self, x):
+		pass
+
+	def maxValue(self,a,b,stepSize):
+		xs= [x for x in np.arange(a,b,stepSize)]
+		y = [self.evaluate(x) for x in xs]
+		return max(y)
+
+class GaussianPDF(PDF):
+
+	def __init__(self,a,b,mu,sigma):
+		PDF.__init__(self,a,b)
+		self.mu=mu
+		self.sigma=sigma
+
+	def evaluate(self, x):
+		coef=1/(self.sigma*(math.sqrt(2*math.pi)))
+		expon=-((x-self.mu)**2)/(2*self.sigma**2)
+		return coef*math.exp(expon)
+
+	def next(self):
+		
+		fromGaussian=False
+		while fromGaussian=False:
+			x1=rand.random()
+			x1=PDF.toRangeAB(self.a,self.b,x1)
+			maxVal=self.maxValue(self.a, self.b, stepSize)
+			y1= self.evaluate(x1)
+			y2=self.toRangeFmax(maxVal, rand.random())
+			if y2<y1:
+				fromGaussian=True
+				return x1
+			else:
+				pass
+
+stepSize = 0.0001
+
+def main():
+	
+	a = -5
+	b = 5
+	
+	sigma = 2
+	mu=0
+	targetValue=1000
+
+	gaus=GaussianPDF(a,b,mu,sigma)
+
+	values=[0 for x in range(0,targetValue)]
+
+	for i in range(0,targetValue):
+		values[i]=gaus.next()
+		print values[i]
+
+if __name__=='__main__':
+	main()
+		
